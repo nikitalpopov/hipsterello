c2cbec7af2a310e5685e8b76c58766355dd88abb
nikitalpopov <tapochek97@ya.ru>
2017-07-18 00:45:17 +0300

    Make Server.js less

--- server/Server.js
+++ server/Server.js
@@ -7,11 +7,6 @@ import { serverPort } from '../src/config.json';
 
 import * as db from './database/DataBaseUtils';
 
-
-import Board from './database/crud/Board';
-import List  from './database/crud/List';
-import Card  from './database/crud/Card';
-
 const app = express();
 
 db.setUpConnection();
@@ -21,113 +16,6 @@ app.use( bp.json() );
 app.use( cors({ origin: '*' }) );
 
 export default app;
-
-// REST API
-
-
-app.get('/board/user/:id', (req, res) => {
-    Board
-        .findBoardByUserId(req.params.id)
-        .then((foundBoard) => {
-            res.send(foundBoard)
-    });
-});
-
-app.post('/board/create', (req, res) => {
-    Board
-        .createBoard(req.body, req.body.userId)
-        .then((createdBoard) => {
-            res.send(createdBoard)
-    });
-});
-
-app.get('/board/:id', (req, res) => {
-    Board
-        .findBoardById(req.params.id)
-        .then((foundBoard) => {
-            res.send(foundBoard)
-    });
-});
-
-app.post('/board/update', (req, res) => {
-    Board
-        .updateBoard(req.body)
-        .then((updatedBoard) => {
-            res.send(updatedBoard)
-    });
-});
-
-app.post('/board/delete', (req, res) => {
-    Board
-        .deleteBoard(req.body)
-        .then((deletedBoard) => {
-            res.send(deletedBoard)
-        });
-});
-
-app.post('/list/create', (req, res) => {
-    List
-        .createList(req.body, req.body.boardId)
-        .then((createdList) => {
-            res.send(createdList)
-    });
-});
-
-app.get('/list/:id', (req, res) => {
-    List
-        .findListById(req.params.id)
-        .then((foundList) => {
-            res.send(foundList)
-        });
-});
-
-app.post('/list/update', (req, res) => {
-    List
-        .updateList(req.body)
-        .then((updatedList) => {
-            res.send(updatedList)
-        });
-});
-
-app.post('/list/delete', (req, res) => {
-    List
-        .deleteList(req.body)
-        .then((deletedList) => {
-            res.send(deletedList)
-        });
-});
-
-app.post('/card/create', (req, res) => {
-    Card
-        .createCard(req.body, req.body.listId)
-        .then((createdCard) => {
-            res.send(createdCard)
-        });
-});
-
-app.get('/card/:id', (req, res) => {
-    Card
-        .findCardById(req.params.id)
-        .then((foundCard) => {
-            res.send(foundCard)
-        });
-});
-
-app.post('/card/update', (req, res) => {
-    Card
-        .updateCard(req.body)
-        .then((updatedCard) => {
-            res.send(updatedCard)
-        });
-});
-
-app.post('/card/delete', (req, res) => {
-    Card
-        .deleteCard(req.body)
-        .then((deletedCard) => {
-            res.send(deletedCard)
-        });
-});
 
 const server = app.listen(serverPort, () => {
     console.log(`Server is up and running on port ${serverPort}`);
