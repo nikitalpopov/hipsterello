0afebb80930bd0900653a06cb567bbd405d72e47
nikitalpopov <tapochek97@ya.ru>
2017-07-24 03:19:14 +0300

    Update Server.js

--- server/Server.js
+++ server/Server.js
@@ -1,6 +1,10 @@
 import express from 'express';
+import session from 'express-session';
 import cors from 'cors';
 import bp from 'body-parser';
+import cp from 'cookie-parser';
+import passport from 'passport';
+import morgan from 'morgan';
 
 import { serverPort } from '../src/config.json';
 
@@ -9,15 +13,26 @@ import * as db from './database/DataBaseUtils';
 const app = express();
 
 db.setUpConnection();
+require('./Passport')(passport);
 
+app.use( morgan('dev') );
+app.use( cp() );
 app.use( bp.json() );
-
 app.use( cors({ origin: '*' }) );
 
-app.use( require('./database/routes/UserRoutes') );
-app.use( require('./database/routes/BoardRoutes') );
-app.use( require('./database/routes/ListRoutes') );
-app.use( require('./database/routes/CardRoutes') );
+app.use( session({ secret: 'hipsterello' }) );
+app.use( passport.initialize() );
+app.use( passport.session() );
+
+// app.use( require('./database/routes/UserRoutes') );
+// app.use( require('./database/routes/BoardRoutes') );
+// app.use( require('./database/routes/ListRoutes') );
+// app.use( require('./database/routes/CardRoutes') );
+
+require( './database/routes/UserRoutes' )(app, passport);
+require( './database/routes/BoardRoutes' )(app, passport);
+require( './database/routes/ListRoutes' )(app, passport);
+require( './database/routes/CardRoutes' )(app, passport);
 
 export default app;
 
