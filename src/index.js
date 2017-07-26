a91402494f105c23ed40fb6d4c63a87d5ec1bbe4
ANTII <4antii@gmail.com>
2017-07-26 18:50:35 +0300

    @4antii design [Not finished]

--- src/index.js
+++ src/index.js
@@ -9,7 +9,8 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 
 import registerServiceWorker from './registerServiceWorker';
 
-import './index.css';
+// import './css/index.css';
+import './css/style.css';
 
 import reducer from './app/AppReducer';
 import App from './app/App';
@@ -24,6 +25,32 @@ ReactDOM.render(
     <Provider store={ store }>
         <Router>
             <div>
+                <nav className="navbar navbar-default">
+                    <div className="container-fluid">
+                        <div className="navbar-header">
+                            <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                                <span className="sr-only">Toggle navigation</span>
+                                <span className="icon-bar"></span>
+                                <span className="icon-bar"></span>
+                                <span className="icon-bar"></span>
+                            </button>
+                        </div>
+
+                        <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                            <ul className="nav navbar-nav">
+                                <li className="menu-elem"><a href=""> <span className="glyphicon glyphicon-th-list"></span> Boards</a></li>
+                            </ul>
+
+
+
+                            <ul className="nav navbar-nav navbar-right">
+                                <li><a href=""><span className="glyphicon glyphicon-user"></span> User Name</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+                <div class="non-transperent"></div>
+
                 <h1>Hipsterello</h1>
                 <Switch>
                     <Route exact path="/" component={ App } />
