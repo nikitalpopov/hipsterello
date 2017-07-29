70a989215594238a48ae803b660392b355761e9a
nikitalpopov <tapochek97@ya.ru>
2017-07-29 23:19:02 +0300

    Update Server.js

--- server/Server.js
+++ server/Server.js
@@ -3,11 +3,9 @@ import session from 'express-session';
 import cors from 'cors';
 import bp from 'body-parser';
 import cp from 'cookie-parser';
-import passport from 'passport';
 import morgan from 'morgan';
 
 import { serverPort } from '../src/config.json';
-
 import * as db from './database/DataBaseUtils';
 
 const app = express();
@@ -28,14 +26,10 @@ app.use(
     })
 );
 
-require('./Passport')(passport);
-app.use( passport.initialize() );
-app.use( passport.session() );
-
-require( './database/routes/UserRoutes' )(app, passport);
-require( './database/routes/BoardRoutes')(app, passport);
-require( './database/routes/ListRoutes' )(app, passport);
-require( './database/routes/CardRoutes' )(app, passport);
+app.use( require('./database/routes/UserRoutes') );
+app.use( require('./database/routes/BoardRoutes') );
+app.use( require('./database/routes/ListRoutes') );
+app.use( require('./database/routes/CardRoutes') );
 
 app.listen(serverPort, () => {
     console.log(`Server is up and running on port ${serverPort}`);
