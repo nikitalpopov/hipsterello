032705e75f3f7e5334a9ac7fc8c0513c4a1f1942
nikitalpopov <tapochek97@ya.ru>
2017-07-30 01:27:45 +0300

    Add PageHeader component

---
+++ src/app/PageHeader.js
@@ -0,0 +1,19 @@
+/**
+ * Created by @nikitalpopov on 30/07/2017.
+ */
+
+import React, { Component } from 'react';
+
+class PageHeader extends Component {
+    render() {
+        return (
+            <div id="padding">
+                <header>
+                    <h1>Hipsterello</h1>
+                </header>
+            </div>
+        )
+    }
+}
+
+export default PageHeader;
